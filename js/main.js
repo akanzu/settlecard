36f1d389cf73698b02bd7346767d7defdd664a95
Semmargl <vlad_1357@mail.ru>
2018-05-04 00:52:33 +0300

    test with simple anim on general sreen and without anim on users sreens

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 if($(window).width() >= '995'){
 	var scene_general = document.getElementById('scene_general');
 	var parallaxInstance = new Parallax(scene_general,{
-		// hoverOnly:true
+		hoverOnly:true
 	});
 
 	var scene_company = document.getElementById('scene_company');
 	var parallaxInstance = new Parallax(scene_company,{
-		// hoverOnly:true
+		hoverOnly:true
 	});
 
 	var scene_goal = document.getElementById('scene_goal');
 	var parallaxInstance = new Parallax(scene_goal,{
-		// hoverOnly:true
+		hoverOnly:true
 	});
 
 	var scene_value = document.getElementById('scene_value');
 	var parallaxInstance = new Parallax(scene_value,{
-		// hoverOnly:true
+		hoverOnly:true
 	});
 
 	var scene_prev_end = document.getElementById('scene_prev-end');
 	var parallaxInstance = new Parallax(scene_prev_end,{
-		// hoverOnly:true
+		hoverOnly:true
 	});
 }
